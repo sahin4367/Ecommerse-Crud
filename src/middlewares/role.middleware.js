438f4765d24e4eecd7247919a53056889e4a1bc8
Sdftlbzd <95076573+Sdftlbzd@users.noreply.github.com>
2024-12-21 01:38:01 +0400

    crud categories

--- src/middlewares/role.middleware.js
+++ src/middlewares/role.middleware.js
@@ -1,5 +1,5 @@
 export const userRole = async (req, res, next) => {
-    if(req.user.role != "admin") {
+    if(req.user.role !== "admin") {
         return res.json("Siz admin deyilsiniz.")
     }
 
